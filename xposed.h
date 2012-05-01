1ec16da37aedc03b97f3bf4d43671a5e4db37492
rovo89 <android@robv.de>
2012-05-01 16:52:28 +0200

    Bumped version

--- xposed.h
+++ xposed.h
@@ -13,7 +13,7 @@ namespace android {
 #define XPOSED_CLASS "de/robv/android/xposed/XposedBridge"
 #define XPOSED_CLASS_DOTS "de.robv.android.xposed.XposedBridge"
 #define XRESOURCES_CLASS "android/content/res/XResources"
-#define XPOSED_VERSION "1.6b1"
+#define XPOSED_VERSION "1.6"
 
 extern bool keepLoadingXposed;
 typedef std::list<Method>::iterator XposedOriginalMethodsIt;
