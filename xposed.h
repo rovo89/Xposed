e8c68d830cf329a54cd7cb0201819fb5c4499919
rovo89 <android@robv.de>
2014-05-10 12:22:40 +0200

    Clean up method declarations in xposed.h

--- xposed.h
+++ xposed.h
@@ -53,15 +53,13 @@ bool xposedSkipSafemodeDelay();
 bool xposedDisableSafemode();
 bool xposedShouldIgnoreCommand(const char* className, int argc, const char* const argv[]);
 bool addXposedToClasspath(bool zygote);
+static void xposedPrepareSubclassReplacement(jclass clazz);
 bool xposedOnVmCreated(JNIEnv* env, const char* className);
 static bool xposedInitMemberOffsets(JNIEnv* env);
 static inline void xposedSetObjectArrayElement(const ArrayObject* obj, int index, Object* val);
 
 // handling hooked methods / helpers
 static void xposedCallHandler(const u4* args, JValue* pResult, const Method* method, ::Thread* self);
-static jobject xposedAddLocalReference(::Thread* self, Object* obj);
-static void replaceAsm(uintptr_t function, unsigned const char* newCode, size_t len);
-static void patchReturnTrue(uintptr_t function);
 static inline bool xposedIsHooked(const Method* method);
 
 // JNI methods
@@ -69,10 +67,15 @@ static jboolean de_robv_android_xposed_XposedBridge_initNative(JNIEnv* env, jcla
 static void de_robv_android_xposed_XposedBridge_hookMethodNative(JNIEnv* env, jclass clazz, jobject reflectedMethodIndirect,
             jobject declaredClassIndirect, jint slot, jobject additionalInfoIndirect);
 static void de_robv_android_xposed_XposedBridge_invokeOriginalMethodNative(const u4* args, JValue* pResult, const Method* method, ::Thread* self);
-static void android_content_res_XResources_rewriteXmlReferencesNative(JNIEnv* env, jclass clazz,
-            jint parserPtr, jobject origRes, jobject repRes);
+static void android_content_res_XResources_rewriteXmlReferencesNative(JNIEnv* env, jclass clazz, jint parserPtr, jobject origRes, jobject repRes);
+static jobject de_robv_android_xposed_XposedBridge_getStartClassName(JNIEnv* env, jclass clazz);
+static void de_robv_android_xposed_XposedBridge_setObjectClassNative(JNIEnv* env, jclass clazz, jobject objIndirect, jclass clzIndirect);
+static void de_robv_android_xposed_XposedBridge_dumpObjectNative(JNIEnv* env, jclass clazz, jobject objIndirect);
+static jobject de_robv_android_xposed_XposedBridge_cloneToSubclassNative(JNIEnv* env, jclass clazz, jobject objIndirect, jclass clzIndirect);
+
 static int register_de_robv_android_xposed_XposedBridge(JNIEnv* env);
 static int register_android_content_res_XResources(JNIEnv* env);
-}
+
+} // namespace android
 
 #endif  // XPOSED_H_
