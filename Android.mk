26447fb0f29867dcf2d56e8aa111c7b13a3f48a6
rovo89 <android@robv.de>
2014-05-11 01:25:24 +0200

    Remove obsolete "xposedtest" binary
    The primary thing that it tested (nuking a method) is no longer needed in
    the main binary after several rounds of refactoring.

--- Android.mk
+++ Android.mk
@@ -42,13 +42,3 @@ LOCAL_MODULE := xposed
 LOCAL_MODULE_STEM := app_process_xposed_sdk$(PLATFORM_SDK_VERSION)
 
 include $(BUILD_EXECUTABLE)
-
-### binary compatibility test executable
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := xposedtest.cpp
-LOCAL_MODULE := xposedtest
-LOCAL_MODULE_STEM := xposedtest_sdk$(PLATFORM_SDK_VERSION)
-LOCAL_MODULE_TAGS := optional
-LOCAL_CFLAGS += -O0 # no optimizations like inline etc.
-include $(BUILD_EXECUTABLE)
-
