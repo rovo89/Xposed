ab86a1205a409a1532982e1c0514458a91946802
rovo89 <android@robv.de>
2017-12-09 09:29:17 +0100

    Clone objects directly to the target class
    Cloning an object with a different size causes some checks to fail. Even
    though the proper target class will be set almost immediately, this
    sitation can occur, especially under high load and with certain debug
    features enabled. So better create the new object with the correct
    class. Requires a change in the ART projects as well.

--- libxposed_art.cpp
+++ libxposed_art.cpp
@@ -149,8 +149,7 @@ jobject XposedBridge_cloneToSubclassNative(JNIEnv* env, jclass, jobject javaObje
     StackHandleScope<3> hs(soa.Self());
     Handle<mirror::Object> obj(hs.NewHandle(soa.Decode<mirror::Object*>(javaObject)));
     Handle<mirror::Class> clazz(hs.NewHandle(soa.Decode<mirror::Class*>(javaClazz)));
-    Handle<mirror::Object> dest(hs.NewHandle(obj->Clone(soa.Self(), clazz->GetObjectSize())));
-    dest->SetClass(clazz.Get());
+    Handle<mirror::Object> dest(hs.NewHandle(obj->Clone(soa.Self(), clazz.Get())));
     return soa.AddLocalReference<jobject>(dest.Get());
 }
 
